ce86ec0c9c8febac43bd9d01b69bdb178cf40f16
Julangar <64862711+Julangar@users.noreply.github.com>
2021-09-07 22:49:43 -0500

    Update Juego.py
    Comentarios añadidos

--- Juego.py
+++ Juego.py
@@ -30,10 +30,10 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+        #Pausa el juego con ENTER
         if event.type == pygame.KEYDOWN:
             pauseRun = not pauseRun
-
+        #Marca la casilla en la que se pulse el clik derecho
         mouseClick = pygame.mouse.get_pressed()
         if sum(mouseClick) > 0:
             posX, posY = pygame.mouse.get_pos()
@@ -50,31 +50,31 @@ while running:
 
             if not pauseRun:
 
-                # Numero de vecinos
+                # Calculamos el Numero de vecinos cercanos
                 nNeigh = status[(x-1)%nX,(y-1)%nY] + status[(x)%nX,(y-1)%nY] + \
                         status[(x+1)%nX,(y-1)%nY] + status[(x-1)%nX,(y)%nY] + \
                         status[(x+1)%nX,(y)%nY] + status[(x-1)%nX,(y+1)%nY] + \
                          status[(x)%nX,(y+1)%nY] + status[(x+1)%nX,(y+1)%nY]
 
-                #Regla 1: Una celula muerta con 3 vecinas "revive"
+                #Regla 1: Una celula muerta con 3 vecinas vivas "revive"
                 if status[x,y] == 0 and nNeigh==3:
                     newStatus[x,y] = 1
 
-                #Regla 2: Una celula viva con mas de 3 vecinos o menos de 2 "muere"
+                #Regla 2: Una celula viva con mas de 3 o menos de 2 vecinas vivas "muere"
                 elif status[x,y] == 1 and (nNeigh < 2 or nNeigh > 3):
                     newStatus[x,y] = 0
 
-
+            #Se crea el poligono de cada celda a dibujar
             poly = [(x*xSize,y*ySize),
                     ((x+1)*xSize,y*ySize),
                     ((x+1)*xSize,(y+1)*ySize),
                     (x*xSize,(y+1)*ySize)]
-
+            #Se dibujan las celdas pra cada par de x e y
             if newStatus[x,y] == 1:
                 pygame.draw.polygon(screen,LIVE_COLOR,poly,0)
             else:
                 pygame.draw.polygon(screen,DEAD_COLOR,poly,1)
-
+    #Se actualiza el estado
     status = np.copy(newStatus)
     time.sleep(0.1)
     pygame.display.flip()
